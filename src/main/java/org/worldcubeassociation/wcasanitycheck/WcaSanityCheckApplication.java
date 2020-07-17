8d1c3129dc8676115920b0a84fc4c89d9b32b2c7
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-07-17 20:11:38 -0300

    Initial structure

---
+++ src/main/java/org/worldcubeassociation/wcasanitycheck/WcaSanityCheckApplication.java
@@ -0,0 +1,13 @@
+package org.worldcubeassociation.wcasanitycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcaSanityCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcaSanityCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/worldcubeassociation/wcasanitycheck/WcaSanityCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.worldcubeassociation.wcasanitycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcaSanityCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
