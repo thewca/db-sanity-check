7e45596d1508c08870577d429f0d09dd325545f2
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-08-21 00:38:53 -0300

    Email template

--- src/main/java/org/worldcubeassociation/dbsanitycheck/service/impl/EmailServiceImpl.java
+++ src/main/java/org/worldcubeassociation/dbsanitycheck/service/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package org.worldcubeassociation.dbsanitycheck.service.impl;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -40,12 +41,15 @@ public class EmailServiceImpl implements EmailService {
 
 			MimeMessage message = emailSender.createMimeMessage();
 
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			boolean multipart = true;
+			MimeMessageHelper helper = new MimeMessageHelper(message, multipart);
 
 			helper.setFrom("acampos@worldcubeassociation.org");
 			helper.setTo(emailTo);
 			helper.setSubject(subject);
-			helper.setText("Done, test");
+
+			boolean html = true;
+			helper.setText(getText(analysis), html);
 
 			// Email the log file
 			FileSystemResource file = new FileSystemResource(new File("log/db-sanity-check.log"));
@@ -56,6 +60,35 @@ public class EmailServiceImpl implements EmailService {
 			log.info("Not sending email");
 		}
 
+	}
+
+	private String getText(Map<String, List<String>> analysis) {
+		StringBuilder sb = new StringBuilder("<h3>Sanity Check Results</h3>\n\n");
+
+		if (analysis.size() == 0) {
+			sb.append("<p>No results to show</p>\n");
+		} else {
+			sb.append("<p>Found inconsistencies in ").append(analysis.size()).append(" topics.</p>\n\n");
+		}
+
+		for (Entry<String, List<String>> item : analysis.entrySet()) {
+			sb.append("<table style=\"border: 1px solid black;\">\n");
+			sb.append(" <thead>\n");
+			sb.append("  <tr><th scope=\"col\" style=\"background-color: #f2f2f2;border: 1px solid black;\">")
+					.append(item.getKey()).append("</th></tr>\n");
+			sb.append(" </thead>\n");
+			sb.append(" <tbody >\n");
+			for (String line : item.getValue()) {
+				sb.append("  <tr>\n");
+				sb.append("   <td style=\"border: 1px solid black;\">\n").append(line).append("</td>\n");
+				sb.append("  </tr>\n");
+			}
+			sb.append(" </tbody>\n");
+			sb.append("</table>\n");
+			sb.append("<br>\n\n");
+		}
+
+		return sb.toString();
 	}
 
 }
